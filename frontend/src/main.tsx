0591fecf7b78a1974a4a179642e7ffeed83cb419
Alen.m <alenmullassery123@gmail.com>
2025-02-21 18:51:45 +0530

    Migrate styles to Tailwind CSS; remove old CSS file and update imports in components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import './assets/styles/index.css'
 import AppRoutes from './presentation/routes/AppRoutes.tsx'
 
 createRoot(document.getElementById('root')!).render(
